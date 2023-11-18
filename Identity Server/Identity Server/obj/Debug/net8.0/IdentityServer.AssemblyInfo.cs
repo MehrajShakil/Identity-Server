739cbe77374231154cbcdbb20e4ec0fd7df1e7ab
MehrajShakil <sisakilcste18@gmail.com>
2023-11-18 09:54:54 +0600

    remove

--- Identity Server/Identity Server/obj/Debug/net8.0/IdentityServer.AssemblyInfo.cs
+++ Identity Server/Identity Server/obj/Debug/net8.0/IdentityServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aebcdb979be89793e8752211783323698c058e30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
